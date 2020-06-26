bd1813e85796029609fcc8b375d051927a00aecd
gitnubl33t <jessicad8695@gmail.com>
2020-06-25 21:02:49 -0700

    Finished Task 5

--- index.js
+++ index.js
@@ -117,10 +117,11 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/) {
-  /*code here*/
+function removeFlavorByName(array, flavor) {
+  array.splice(array.indexOf(flavor), 1);
+  console.log(array);
 }
-
+removeFlavorByName(originalFlavors, "Vanilla");
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
 Your function should accept: 
