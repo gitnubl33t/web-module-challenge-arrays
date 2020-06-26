7e1a4f9bff86e3de566340cdf9828fcdc4bc0787
gitnubl33t <jessicad8695@gmail.com>
2020-06-25 18:26:49 -0700

    Finished Task 3

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function is31Flavors(array) {
   }
 }
 
-is31Flavors(originalFlavors);
+// is31Flavors(originalFlavors);
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -66,8 +66,11 @@ Your function should add the flavor to the front of the array and console.log th
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */
 
 function addFlavor(flavor, array) {
-  /*code here*/
+  array.unshift(flavor);
+  console.log(array);
 }
+
+// addFlavor("Rainbow Sherbert", originalFlavors);
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
@@ -79,9 +82,12 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/
 
-function removeLastFlavor(/*code here*/) {
-  /*code here*/
+function removeLastFlavor(array) {
+  array.pop();
+  console.log(array);
 }
+
+removeLastFlavor(originalFlavors);
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
